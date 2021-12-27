d630cd7f00cec86784f98230334f86ac26e2cd73
twilightzone-finance <94212049+twilightzone-finance@users.noreply.github.com>
2021-12-27 16:21:20 -0500

    Update countdown.js

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -3,24 +3,8 @@
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
-
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
-  let today = new Date(),
-      dd = String(today.getDate()).padStart(2, "0"),
-      mm = String(today.getMonth() + 1).padStart(2, "0"),
-      yyyy = today.getFullYear(),
-      nextYear = yyyy + 1,
-      dayMonth = "01/27/",
-      birthday = dayMonth + yyyy;
   
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > birthday) {
-    birthday = dayMonth + nextYear;
-  }
-  //end
-  
-  const countDown = new Date(birthday).getTime(),
+  const countDown = new Date(1643216400000).getTime(),
       x = setInterval(function() {    
 
         const now = new Date().getTime(),
